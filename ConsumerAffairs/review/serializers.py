0c9797b76e138b3d9eb63b2c5e4e6f71d09fb1b3
Sailesh Dhungana <dhunganasailesh@gmail.com>
2019-04-29 05:49:34 +0545

    Serializer changed to not show user and ip to the reviewer

--- ConsumerAffairs/review/serializers.py
+++ ConsumerAffairs/review/serializers.py
@@ -2,25 +2,18 @@ from .models import Review, Company
 from rest_framework import serializers
 from django.contrib.auth.models import User
 
-class UserSerializer(serializers.ModelSerializer):
-
-	class Meta:
-		model = User
-		fields = ('id', 'username', 'email', 'first_name', 'last_name')
-
 class CompanySerializer(serializers.ModelSerializer):
 
 	class Meta:
 		model = Company
-		fields = '__all__'
+		fields = ('id', 'name', 'description', 'url')
 
 class ReviewRetrieveSerializer(serializers.ModelSerializer):
 	company = CompanySerializer(many=False, read_only=True)
-	reviewer = UserSerializer(many=False, read_only=True)
 
 	class Meta:
 		model = Review
-		fields = '__all__'
+		fields = ('id', 'rating', 'title', 'summary', 'submitted_date', 'company')
 
 class ReviewCreateSerializer(serializers.ModelSerializer):
 
